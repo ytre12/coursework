d08642955ab4ddba15bc2d69f5d03eec626c0fdc
ytre12 <obio12yt@gmail.com>
2026-03-03 20:04:52 +0200

    Add funk to delete and change data in db + show all forest in admmin panel

--- app.py
+++ app.py
@@ -46,6 +46,38 @@ def admin():
 
     return render_template('admin.html')
 
+@app.route('/change', methods=['POST', 'GET'])
+def change():
+    forest_db = Forest.query.all()
+
+    return render_template('change.html', forests=forest_db)
+
+@app.route('/delete/<int:id>', methods=['POST', 'GET'])
+def delete(id):
+    forest = Forest.query.get_or_404(id)
+    forest_db.session.delete(forest)
+    forest_db.session.commit()
+    return redirect('/change')
+
+@app.route('/edit/<int:id>', methods=['POST', 'GET'])
+def edit(id):
+    forest = Forest.query.get_or_404(id)
+    if request.method == 'POST':
+        forest.mainForest = request.form['mainForest']
+        forest.forest = request.form['forest']
+        forest.typeCutting = request.form['typeCutting']
+        forest.quarter = request.form['quarter']
+        forest.department = request.form['department']
+        forest.area = request.form['area']
+        forest.volumeForestManagement = request.form['volumeForestManagement']
+        forest.month = request.form['month']
+        forest.decade = request.form['decade']
+        forest.year = request.form['year']
+
+        forest_db.session.commit()
+        return redirect('/change')
+
+    return render_template('edit.html', forest=forest)
 
 
 if __name__ == '__main__':    
