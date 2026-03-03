49c4c4964510dd3cc5cab9f7b7f7a81eae2fa54c
ytre12 <obio12yt@gmail.com>
2026-03-03 18:46:57 +0200

    Add funk (add new forest in forest_Db), add forest form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from forest_DB import forest_db, Forest
 
 app = Flask(__name__)
@@ -13,9 +13,39 @@ def main():
 
     return render_template('index.html', forests=forestsDb)
 
-@app.route('/admin')
+@app.route('/admin', methods=['GET', 'POST'])
 def admin():
+    if request.method == 'POST':
+        mainForest = request.form['mainForest']
+        forest = request.form['forest']
+        typeCutting = request.form['typeCutting']
+        quarter = request.form['quarter']
+        department = request.form['department']
+        area = request.form['area']
+        volumeForestManagement = request.form['volumeForestManagement']
+        month = request.form['month']
+        decade = request.form['decade']
+        year = request.form['year']
+
+        new_forest = Forest(
+            mainForest=mainForest, 
+            forest=forest, 
+            typeCutting=typeCutting, 
+            quarter=quarter, 
+            department=department, 
+            area=area, 
+            volumeForestManagement=volumeForestManagement, 
+            month=month, 
+            decade=decade, 
+            year=year
+        )
+
+        forest_db.session.add(new_forest)
+        forest_db.session.commit()
+        return redirect('/admin')
+
     return render_template('admin.html')
+
 
 
 if __name__ == '__main__':    
